cd34dbc7fecbb5c62db2f6f57d5cae9befeb02cc
GLOBAL\abdulalem.alsanabani <aleem.asnabani@gmail.com>
2023-09-18 17:33:04 +0300

    testing sync btw local and remote

--- code2.R
+++ code2.R
@@ -2,3 +2,9 @@ X<- 10
 Y<- 100
 Z<- X*Y
 paste("X+Y=", z )
+
+my_installed_pack<- installed.packages()
+nrow(my_installed_pack)
+names(my_installed_pack)
+class(my_installed_pack)
+my_installed_pack[1,]
